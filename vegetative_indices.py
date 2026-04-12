7753415c6f7152a6f5ff4ee7e18bb650c70b7dd0
simondparsons <s.d.parsons@gmail.com>
2026-04-12 16:25:26 +0100

    GPU fixed?

--- vegetative_indices.py
+++ vegetative_indices.py
@@ -395,17 +395,17 @@ def rgb_to_hsv(r, g, b):
     # OpenCV expects BGR and values in range [0,255]
     if GPU_AVAILABLE:
         print("Convert to uint8")
-        r = np.uint8(r) 
-        g = np.uint8(b)
-        b = np.uint8(g)
+        r = cp.uint8(r.get()) 
+        g = cp.uint8(b.get())
+        b = cp.uint8(g.get())
         bgr_pixel = np.stack([b, g, r], axis=2)
         print("Calling open CV")
         hsv_pixel = cv.cvtColor(bgr_pixel, cv.COLOR_BGR2HSV)
         h, s, v = hsv_pixel[0][0]
         print("Convert back to float64")
-        h = np.float64(h.get()) 
-        s = np.float64(s.get())
-        v = np.float64(v.get())
+        h = cp.float64(h.get()) 
+        s = cp.float64(s.get())
+        v = cp.float64(v.get())
     else:
         bgr_pixel = np.uint8([[[b, g, r]]])
         hsv_pixel = cv.cvtColor(bgr_pixel, cv.COLOR_BGR2HSV)
